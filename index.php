1e5d785a0f9020b410fe12b468efe866ef9a92ae
haorou <haorou@live.fr>
2018-09-20 17:18:56 +0200

    ajout create user / connect

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
         $e->getMessage();
     }
 
-    #Function 
     function issetAndNotEmpty($variable)
     {
         $isIssetAndNotEmpty = False;
@@ -18,7 +17,23 @@
         return $isIssetAndNotEmpty;
     }
 
+    #CREATE USER
+    if(issetAndNotEmpty($_POST("new_pseudo")) && issetAndNotEmpty($_POST("new_pass")) 
+    && issetAndNotEmpty($_POST("new_email")))
+    {
+        $createUser = $bdd->prepare("INSERT INTO utilisateur(pseudo, pass, email) 
+                                    VALUES (:pseudo, :pass, :email)");
+        $createUser->execute(array(
+                            "pseudo" => $_POST("new_pseudo"),
+                            "pass" => $_POST("new_pass"),
+                            "email" => $_POST("new_email")));
+    }
 
+    #CONNECTER USER
+    if(issetAndNotEmpty($_POST("pseudo")) && issetAndNotEmpty($_POST("pass")))
+    {
+
+    }
 
 
 
